f83fbe49876afa477cc15ca830348b9e35da8e78
Sudhan1112 <sudhansudhan83@gmail.com>
2024-08-16 10:01:28 +0530

    changes done is folder structure

--- salary.h
+++ salary.h
@@ -4,20 +4,27 @@
 using namespace std;
 
 class Salary {
-private:
-    double base; // Base Salary
-    double bonus; // Bonus
-    double tax;   // Tax Rate
+    private:
+        double base; // Base Salary
+        double bonus; // Bonus
+        double tax;   // Tax Rate
 
-public:
-    // Constructor
-    Salary(double base, double bonus, double tax);
+    public:
+        // Constructor
+        Salary(double base, double bonus, double tax): base(base), bonus(bonus), tax(tax) {};
 
-    // Method to calculate net salary
-    double calculateNetSalary() const;
+        // Method to calculate net salary
+        double calculateNetSalary() const {
+            double taxDeduction = base * tax / 100;
+            double netSalary = base - taxDeduction + bonus;
+            return netSalary;
+        };
 
-    // Method to print salary details
-    void printSalaryDetails() const;
+        // Method to print salary details
+        void printSalaryDetails() const {
+              cout << "Base Salary: " << base << "\nBonus: " << bonus
+         << "\nTax Rate: " << tax << "%" << endl;
+        };
 };
 
 #endif // SALARY_H
